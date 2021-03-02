f931c6d1c06a459f13d747f9262e4b46bdd2f498
DK <dekaprimatio@gmail.com>
2021-03-02 10:48:55 +0700

    Animasi Drawable

--- Week6_32864_B/app/src/main/java/umn/ac/id/week6_32864_b/MainActivity.java
+++ Week6_32864_B/app/src/main/java/umn/ac/id/week6_32864_b/MainActivity.java
@@ -2,13 +2,26 @@ package umn.ac.id.week6_32864_b;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    AnimationDrawable animasiKuda;
+    ImageView gambarKuda;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        gambarKuda = (ImageView) findViewById(R.id.gambarKuda);
+        gambarKuda.setBackgroundResource(R.drawable.kuda_lari);
+        animasiKuda = (AnimationDrawable) gambarKuda.getBackground();
+        gambarKuda.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                animasiKuda.start();
+            }
+        });
     }
 }

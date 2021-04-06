d17226186a76573fd2e0f28762e27e3d00a09f79
DK <dekaprimatio@gmail.com>
2021-04-06 11:52:14 +0700

    Week9 masih bug

---
+++ Week9_32864/app/src/main/java/umn/ac/id/week9_32864/DetilActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.week9_32864;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detil);
+    }
+}
